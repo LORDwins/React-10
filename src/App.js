36023e0c7e3d7280c6acccc1143ee59b64c58745
LORDwins <lordwenramoso650@gmail.com>
2026-01-21 14:26:15 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Employee from './components/Employee.js';
+import { useState } from 'react';
+
+
+function App() {
+const [role, setRole] = useState('GIS Analyst');
+const showEmployees = true;
+return (
+<div className="App">
+{console.log('inside the return')}
+
+29
+
+{showEmployees ? (
+<>
+<input type= 'text' onChange={(e) =>{
+console.log(e.target.value);
+setRole(e.target.value);
+}}/>
+<Employee name= "Diana" role= "Engineer" experience= "5 years"/>
+<Employee name= "John" role= {role}/>
+<Employee name= "Aemi"/>
+</>
+) : (
+<p>You cannot see the Employees</p>
+)}
+</div>
+);
+}
+export default App;
